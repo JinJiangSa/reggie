ac48b0cc86f0473ef2363f001da44b0b76e39aa9
JinjiangSa <2265138803@qq.com>
2022-06-09 11:38:53 +0800

    Git初体验2

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -42,7 +42,6 @@ public class EmployeeController {
         LambdaQueryWrapper<Employee> lqw = new LambdaQueryWrapper();
         lqw.eq(Employee::getUsername, employee.getUsername());
         Employee emp = employeeService.getOne(lqw);
-        System.out.println("abc");
 
         //3、如果没有查询到则返回登录失败结果
         if (emp == null) {
